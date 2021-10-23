3cd8e27769c922211d47cadbbbd002d29869ae2f
oscarlappay <91309474+oscarlappay@users.noreply.github.com>
2021-10-23 12:05:42 +1100

    view individual user and view individual user schedule

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const morgan = require('morgan')
 const data = require('./data')
 const app = express() // invoke express in order to create an instance
 
@@ -9,53 +10,85 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Logging middleware
+app.use(morgan('dev'))
+
+// Set view engine
+app.set('view engine', 'ejs')
+
+// Set static folder
+app.use(express.static('public'))
+
 // ROUTES
-// Welcome
+// Welcome 
 app.get('/', (req, res) => {
-  res.send("Welcome to our schedule website")
+  res.render('pages/home')
 })
 
 // Get all users
 app.get('/users', (req, res) => {
-  res.send(data.users)
+  res.render('pages/users', {
+    users: data.users
+  })
 })
 
 // Get all schedules
 app.get('/schedules', (req, res) => {
-  res.send(data.schedules)
+  res.render('pages/schedules', {
+    schedules: data.schedules
+  })
+})
+
+// Get new user
+app.get('/users/new', (req, res) => {
+  res.render('pages/new-user')
+})
+
+//Get new schedule
+app.get('/schedules/new', (req, res) => {
+  res.render('pages/new-schedule')
 })
 
 // Get individual user
 app.get('/users/:id', (req, res) => {
-  const user = data.users[req.params.id]
-  res.send(user)
+  res.render('pages/user',{
+    id:req.params.id,
+    users : data.users
+  })
 })
 
+
 //Get user's schedules
-app.get('/users/:id/schedules', (req,res) => {
+app.get('/schedules/:id', (req,res) => {
   const uschedule = data.schedules.filter(schedule => schedule.user_id == Number(req.params.id))
-  res.send(uschedule)  
+  res.render('pages/schedule',{
+    id:req.params.id,
+    schedules : uschedule
+  })
+      
 })
 
 // Create new schedules
 app.post('/schedules', (req, res) => {
   // TODO: Validate data
 
+
   //Add new schedules to all schedules
   data.schedules.push(req.body)
-  res.send(req.body)
+  res.redirect('/schedules')
 })
 
 // Create new user
 app.post('/users', (req, res) => {
+
+  
   // Using bcryptjs
   const password = req.body.password
   const salt = bcrypt.genSaltSync(10)
   const hash = bcrypt.hashSync(password, salt)
   req.body.password = hash
-  // TODO: Add hash to user object and then push to user array
   data.users.push(req.body)
-  res.send(req.body)
+  res.redirect('/users')
 })
 
 app.listen(PORT, () => {
